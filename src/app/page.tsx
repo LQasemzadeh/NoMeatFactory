1d3bfe307b70f4c78a1fe18383a87a7a4a0f8d1f
lqasemzadeh <lqasemzadeh@gmail.com>
2024-12-17 20:43:45 +0100

    Remove Adsbar and Apart components from the app page
    Adsbar and Apart components were removed as they are no longer needed for the page structure. This simplifies the layout and improves maintainability of the codebase.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import AboutUs from "@/app/Components/AboutUs";
 import Banner from "@/app/Components/Banner";
 import Products from "@/app/Components/Products";
 import Footer from "@/app/Components/Footer/Footer";
-import Adsbar from "@/app/Components/Adsbar";
 import Contact from "@/app/Components/Contact";
-import Apart from "@/app/Components/Apart";
 import Services from "@/app/Components/Services";
 
 
@@ -21,7 +19,6 @@ export default function Home() {
   <Banner />
   <Services />
   <Products />
-  <Adsbar />
   <Contact />
   <Footer />
 
